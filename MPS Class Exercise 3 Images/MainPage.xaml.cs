1ed6491119f772c76ffe6c8fc78bffe1654ee216
D3CR1PT3R <47520842+vicken999@users.noreply.github.com>
2020-11-16 13:55:57 +0200

    first commit

---
+++ MPS Class Exercise 3 Images/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MPS_Class_Exercise_3_Images
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
